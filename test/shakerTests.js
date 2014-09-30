50d5628f63c970593e86e8a81f65ff0a0d38052b
Golo Roden <webmaster@goloroden.de>
2014-09-30 11:19:07 +0200

    Upgraded code 2014. Renamed to shaker.

--- test/shakerTests.js
+++ test/shakerTests.js
@@ -2,26 +2,28 @@
 
 var assert = require('node-assertthat');
 
-var password = require('../lib/password');
+var shaker = require('../lib/shaker');
 
-suite('password', function () {
+suite('shaker', function () {
   suite('generate', function () {
     test('runs callback.', function (done) {
-      password.generate('', function () {
+      shaker.generate('', function () {
         done();
       });
     });
 
-    test('creates a random 100 characters salt.', function (done) {
-      password.generate('', function (salt) {
+    test('creates a random 128 characters salt.', function (done) {
+      shaker.generate('', function (err, salt) {
+        assert.that(err, is.null());
         assert.that(salt, is.ofType('string'));
-        assert.that(salt.length, is.equalTo(100));
+        assert.that(salt.length, is.equalTo(128));
         done();
       });
     });
 
     test('calculates a 64 hex-characters hash.', function (done) {
-      password.generate('', function (salt, hash) {
+      shaker.generate('', function (err, salt, hash) {
+        assert.that(err, is.null());
         assert.that(hash, is.ofType('string'));
         assert.that(/^[0-9a-f]{64}$/i.test(hash), is.true());
         done();
@@ -31,15 +33,18 @@ suite('password', function () {
 
   suite('verify', function () {
     test('returns false when password, salt and hash do not match each other.', function (done) {
-      password.verify('secret', 'this is an invalid salt', 'this is an invalid hash', function (verified) {
+      shaker.verify('secret', 'this is an invalid salt', 'this is an invalid hash', function (err, verified) {
+        assert.that(err, is.null());
         assert.that(verified, is.false());
         done();
       });
     });
 
     test('returns false when the password does not match the given salt and hash.', function (done) {
-      password.generate('secret', function (salt, hash) {
-        password.verify('another secret', salt, hash, function (verified) {
+      shaker.generate('secret', function (err, salt, hash) {
+        assert.that(err, is.null());
+        shaker.verify('another secret', salt, hash, function (err, verified) {
+          assert.that(err, is.null());
           assert.that(verified, is.false());
           done();
         });
@@ -47,8 +52,10 @@ suite('password', function () {
     });
 
     test('returns true when password, salt and hash match each other.', function (done) {
-      password.generate('secret', function (salt, hash) {
-        password.verify('secret', salt, hash, function (verified) {
+      shaker.generate('secret', function (err, salt, hash) {
+        assert.that(err, is.null());
+        shaker.verify('secret', salt, hash, function (err, verified) {
+          assert.that(err, is.null());
           assert.that(verified, is.true());
           done();
         });
